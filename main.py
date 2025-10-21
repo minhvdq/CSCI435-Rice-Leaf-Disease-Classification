159d493ae61f3768203a659d7aa834470e4ee730
Damian@Vu <minhvdq@gmail.com>
2025-10-21 12:48:42 +1100

    add default

--- main.py
+++ main.py
@@ -580,5 +580,8 @@ if __name__ == "__main__":
     parser.add_argument('--pretrained', type=bool, default=True)
     parser.add_argument('--default', type=bool, default=False)
     args = parser.parse_args()
-    pretrained = args.pretrained
+    if args.pretrained:
+        pretrained = args.pretrained
+    if args.default:
+        default = args.default
     train()
